66bc175de70e81537164fbf06f776b5c116bfe2d
ndossett <niki.m.dossett@gmail.com>
2020-10-02 11:39:58 -0400

    finished header

--- components/Header.js
+++ components/Header.js
@@ -11,4 +11,27 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+function Header() {
+    const header1 = document.createElement('header')
+    const dateSpan = document.createElement('span')
+    const h1 = document.createElement('h1')
+    const tempSpan = document.createElement('span')
+
+    header1.classList.add('header')
+    dateSpan.classList.add('date')
+    tempSpan.classList.add('temp')
+
+    header1.appendChild(dateSpan)
+    header1.appendChild(h1)
+    header1.appendChild(tempSpan)
+
+    dateSpan.textContent = ('March 28, 2020')
+    h1.textContent = ('Lambda Times')
+    tempSpan.textContent = ('98°')
+
+    return header1
+
+}
+
+const headerContainer = document.querySelector('.header-container')
+headerContainer.appendChild(Header())
